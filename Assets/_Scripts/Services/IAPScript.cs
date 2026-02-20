10b2b903f9591bd15f95c8d07b376701128ea15e
sergiodias25 <sergio.da.dias@gmail.com>
2026-02-20 16:46:58 +0000

    Watch ad an dpurchase in all buttons text

--- Assets/_Scripts/Services/IAPScript.cs
+++ Assets/_Scripts/Services/IAPScript.cs
@@ -1,4 +1,5 @@
 using Assets.Scripts.AnalyticsEvent;
+using GridSum.Assets._Scripts.Visuals;
 using UnityEngine;
 using UnityEngine.Purchasing;
 
@@ -54,13 +55,13 @@ public class IAPScript : MonoBehaviour
             case SUNRISE_THEME:
                 _gameManager.SavedGameData.EnableSunriseTheme();
                 _gameManager.SavedGameData.PersistData();
-                GameObject.Find("SunrisePurchaseIcon").SetActive(false);
+                FindObjectOfType<HideIcons>().Process();
                 _settingsHandler.ChangeTheme(2);
                 break;
             case SUNSET_THEME:
                 _gameManager.SavedGameData.EnableSunsetTheme();
                 _gameManager.SavedGameData.PersistData();
-                GameObject.Find("SunsetPurchaseIcon").SetActive(false);
+                FindObjectOfType<HideIcons>().Process();
                 _settingsHandler.ChangeTheme(3);
                 break;
             default:
@@ -88,12 +89,8 @@ public class IAPScript : MonoBehaviour
             switch (product.definition.id)
             {
                 case SUNRISE_THEME:
-                    _settingsHandler.RevertTheme();
-                    GameObject.Find("SunrisePurchaseIcon").SetActive(false);
-                    break;
                 case SUNSET_THEME:
                     _settingsHandler.RevertTheme();
-                    GameObject.Find("SunsetPurchaseIcon").SetActive(false);
                     break;
             }
         }
--- Assets/_Scripts/Visuals/HideIcons.cs
+++ Assets/_Scripts/Visuals/HideIcons.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Globalization;
+using Assets.SimpleLocalization.Scripts;
+using TMPro;
 using UnityEngine;
 
 namespace GridSum.Assets._Scripts.Visuals
@@ -10,23 +14,38 @@ namespace GridSum.Assets._Scripts.Visuals
         [SerializeField]
         private GameObject _sunsetIcon;
 
+        [SerializeField]
+        private TextMeshProUGUI _sunriseText;
+
+        [SerializeField]
+        private TextMeshProUGUI _sunsetText;
+
         void OnEnable()
+        {
+            Process();
+        }
+
+        public void Process()
         {
             if (FindObjectOfType<GameManager>().SavedGameData.PurchaseData.SunriseTheme)
             {
                 _sunriseIcon.SetActive(false);
+                _sunriseText.text = LocalizationManager.Localize("btn-shop-sunrisetheme-purchased");
             }
             else
             {
                 _sunriseIcon.SetActive(true);
+                _sunriseText.text = LocalizationManager.Localize("btn-shop-sunrisetheme");
             }
             if (FindObjectOfType<GameManager>().SavedGameData.PurchaseData.SunsetTheme)
             {
                 _sunsetIcon.SetActive(false);
+                _sunsetText.text = LocalizationManager.Localize("btn-shop-sunsettheme-purchased");
             }
             else
             {
                 _sunsetIcon.SetActive(true);
+                _sunsetText.text = LocalizationManager.Localize("btn-shop-sunsettheme");
             }
         }
     }
